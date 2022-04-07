0daa4f27882a5f4d7dab793c3de0d63377ff1a2b
integralxdx <starbruce1@gmail.com>
2022-04-07 09:18:27 -0700

    c is here

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-
+#include <stdio.h> 
+/**
+ *main - we use the main function
+ *return:  0 after printing
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+/**
+ *main - Starting function
+ *return : to return 0
+ *printf: print func
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
