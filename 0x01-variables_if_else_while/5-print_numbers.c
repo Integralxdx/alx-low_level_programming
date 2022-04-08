ed662f35c1a77ab597c7b1e4ce92f2d3ee41f4a4
integralxdx <starbruce1@gmail.com>
2022-04-08 09:20:14 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - where all code will run through
+ * Return: return 0 always
+ * Description: Prints number 0-9
+ * num: number from 0-9
+ */
+
+int main(void)
+
+{
+
+int num;
+
+for (num = 0; num < 10; ++num)
+
+putchar(num);
+
+putchar('\n');
+
+return (0);
+
+}
