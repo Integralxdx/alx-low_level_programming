7ce46491c70ca0f47769bc221a21a0c6d3cc580a
integralxdx <starbruce1@gmail.com>
2022-04-12 15:01:49 -0700

    2-7

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10  - A function that prints the alphabets 10 times
+ */
+void print_alphabet_x10(void)
+
+{
+
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+
+	{
+
+		for (j = 'a'; j <= 'z'; j++)
+
+		{
+
+			_putchar(j);
+
+		}
+
+		_putchar('\n');
+
+	}
+
+}
