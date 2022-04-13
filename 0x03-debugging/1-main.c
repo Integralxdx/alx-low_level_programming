5d1de99f87724287b34b06ea5511b929ef60d292
integralxdx <starbruce1@gmail.com>
2022-04-13 10:09:36 -0700

    1th

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,7 +2,7 @@
 
 /**
  * main - causes an infinite loop
- * return: 0
+ * Return: 0
  */
 
 int main(void)
@@ -10,7 +10,7 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-/**
+/*
 *	i = 0;
 *
 *	while (i < 10)
