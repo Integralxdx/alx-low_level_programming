3338ec38e6e6e86a8f4259ce3ba0e29665d86e34
integralxdx <starbruce1@gmail.com>
2022-04-14 10:31:00 -0700

    3rd

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,17 @@
 
 /**
  * print_numbers - A func to print 0-9 followed by new line
+ * Return: 0
  */
 
 void print_numbers(void)
 {
 	int a;
 
-	for (a = '0'; a <= '9'; a++)
-	{
-		_putchar(a);
-	}
+	a = 0;
+
+	for (a = 0; a < 10; a++)
+
+	_putchar(a + '0');
 	_putchar('\n');
 }
