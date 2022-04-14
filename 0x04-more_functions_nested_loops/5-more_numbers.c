5185d3c9c3ce78279c122ddefb901b9f03f78e6b
integralxdx <starbruce1@gmail.com>
2022-04-14 12:34:16 -0700

    5th

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,22 +7,22 @@
 
 void more_numbers(void)
 {
-	int a, b, c;
+int a, b, c;
 
-	for (a = 0; a <= 1; a++)
-	{
-	for (b = 0; b <= 9; b++)
-	{
-	for (c = 0; c < 10; c++)
-	{
-	if (!(a == 1 && b >= 5))
-	{
-	if (a)
-	_putchar(a + '0');
-	_putchar(b + '0');
-	}
-	}
-	}
-	_putchar('\n');
-	}
+for (c = 0; c < 10; c++)
+{
+for (a = 0; a <= 1; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+if (!(a == 1 && b >= 5))
+{
+if (a)
+_putchar(a + '0');
+_putchar(b + '0');
+}
+}
+}
+_putchar('\n');
+}
 }
