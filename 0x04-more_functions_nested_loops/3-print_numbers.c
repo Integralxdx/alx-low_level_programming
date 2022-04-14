2249180249290a6913cc2923d687c9c20ad097e6
integralxdx <starbruce1@gmail.com>
2022-04-14 10:38:24 -0700

    3rd

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -7,12 +7,13 @@
 
 void print_numbers(void)
 {
-	int a;
 
-	a = 0;
+int i;
 
-	for (a = 0; a < 10; a++)
+i = 0;
 
-	_putchar(a + '0');
-	_putchar('\n');
+for (i = 0; i < 10; i++)
+
+_putchar(i + '0');
+_putchar('\n');
 }
