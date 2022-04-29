a4dba458480424f5312c660927ee26fd397b6286
Integralxdx <97835332+Integralxdx@users.noreply.github.com>
2022-04-29 10:36:47 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,2 +1,47 @@
 #include "main.h"
 
+/**
+ *evaluate_num - recursion loop
+ *@num: num
+ *@i: iterator no
+ *Return: 0 or 1
+ */
+
+int evaluate_num(int num, int i)
+{
+	if (i == num - 1)
+	{
+		return (1);
+	}
+	else if (num % i == 0)
+	{
+		return (0);
+	}
+	if (num % i != 0)
+	{
+		return (evaluate_num(num, i + 1));
+		}
+	return (0);
+}
+
+/**
+ *is_prime_number - funct to evaluate prime no
+ *@num: number to check
+ *Return: return 1 if prime 0 for other
+ */
+
+int is_prime_number(int num)
+{
+	int i;
+	
+	i = 2;
+	if (num < 2)
+	{
+		return (0);
+	}
+	if (num == 2)
+	{
+		return (1);
+	}
+	return (evaluate_num(num, i));
+}
